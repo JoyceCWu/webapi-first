39499cad107187374fd8e8d18ec63e78d2fe04ff
Joy Huang <jhuang@tradestation.com>
2013-05-30 13:38:23 -0500

    test commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+
+
+var stock={"symbol":[
+        {
+            "Symbol":"MSFT",
+            "SymbolRoot":"MSFT",
+            "Description":"Microsoft Corp",
+            "AssetType":"STOCK",
+            "Exchange":"NASDAQ",
+            "NetChange":0.12
+        },
+
+        {
+            "Symbol":"MSFT",
+            "SymbolRoot":"MSFT",
+            "Description":"Microsoft Corp",
+            "AssetType":"STOCK",
+            "Exchange":"NASDAQ",
+            "NetChange":0.12
+        }
+]}
+
+
+console.log(stock);
+
+
+/*
+	$.getJSON('http://localhost/webapi/ajax/test.json', function(data) {
+
+	  console.log( "success" );
+	  var items = [];
+		alert("document ready occurred!"); 
+
+		$.each(data, function(items) {
+
+	    console.log(items);
+	  });
+
+
+	});
+*/
