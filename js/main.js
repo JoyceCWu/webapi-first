140edf0f01e437328cf104ef21157a0f4a8fda5b
Joy Huang <jhuang@tradestation.com>
2013-06-04 13:35:25 -0500

    Final commit for sample WebAPI app

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-// Testing JSon Data 
-
 $(function() {
     var extractToken = function(hash) {
             var match = hash.match(/access_token=(\w+)/);
@@ -19,102 +17,16 @@ $(function() {
 		}, "json");
 
 		return beers;
-		
-		// return ko.utils.arrayFilter(beers, function(beer) {
-		// 	return beer.Name.toLowerCase().indexOf(search) >= 0;
-		// });
 	}, viewModel);
 
 	ko.applyBindings(viewModel);
-
-	// function createList(beers){
-	// 	// iterate over beers
-
-	// }
-
-	// function create(beer){
-	// 	var model = {
-	// 		beer:beer,
-	// 		collapse: function(currentElement,uiob){
-	// 			$(".collapse").collapse()
-
-	// 		}
-
-	// 	}
-	// 	return model;
-
-	// }
 });
 
-
-
-
-// Testing Charting
-
-$(function() {
-    $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=new-intraday.json&callback=?', function(data) {
-
-        // create the chart
-        $('#container-stock').highcharts('StockChart', {
-
-            xAxis: {
-                gapGridLineWidth: 0
-            },
-            
-            rangeSelector : {
-                buttons : [{
-                    type : 'hour',
-                    count : 1,
-                    text : '1h'
-                }, {
-                    type : 'day',
-                    count : 1,
-                    text : '1D'
-                }, {
-                    type : 'all',
-                    count : 1,
-                    text : 'All'
-                }],
-                selected : 1,
-                inputEnabled : false
-            },
-            
-            series : [{
-                name : 'AAPL',
-                type: 'area',
-                data : data,
-                gapSize: 5,
-                tooltip: {
-                    valueDecimals: 2
-                },
-                fillColor : {
-                    linearGradient : {
-                        x1: 0, 
-                        y1: 0, 
-                        x2: 0, 
-                        y2: 1
-                    },
-                    stops : [[0, Highcharts.getOptions().colors[0]], [1, 'rgba(0,0,0,0)']]
-                },
-                threshold: null
-            }]
-        });
-    });
-});
-
-
-// $('#demoing').click(function () {
-//     console.log("boo");
-//  $('demo').remove();
-// });
 
 $("#appendedInput").click(function () {
-    console.log("as;ldfk");
- //   $("#remove").addClass("fadeOut");
     $(".demo").remove();
 });
 
 $("#plusle").click(function () {
-    console.log("booooo");
     $(".accordion").remove();
 });
